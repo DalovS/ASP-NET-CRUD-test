ed7bdcec6b2853bea901f3b0c2e0e8d1177cab09
Sabri Dalov <123773953+DalovS@users.noreply.github.com>
2024-03-02 13:34:40 +0200

    Create Client.cs
    New fields.

---
+++ CarManagementApp/CarManagementApp/Models/Client.cs
@@ -0,0 +1,9 @@
+namespace CarManagementApp.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
